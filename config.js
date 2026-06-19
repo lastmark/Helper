069e68c6c9ad2b04ebe9e3ffc93a21d7a33a974e
lastmark <par337187@gmail.com>
2026-06-19 06:05:37 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  token: process.env.HELPER_TOKEN,
+  clientId: process.env.HELPER_CLIENT_ID,
+  devId: process.env.DEV_ID || "1303357369622990889",
+  redisUrl: process.env.REDIS_URL || 'redis://localhost:6379'
+};
