c020bbb15d3e59c68070d31afdbdfece42688645
lastmark <par337187@gmail.com>
2026-06-19 06:23:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 module.exports = {
   token: process.env.HELPER_TOKEN,
-  clientId: process.env.HELPER_CLIENT_ID,
+  clientId: process.env.HELPER_CLIENT_ID || "1517368811609788488",
   devId: process.env.DEV_ID || "1303357369622990889",
   redisUrl: process.env.REDIS_URL || 'redis://default:IujhJsEZHqzRuIukNKiOGGfRsUlpmlcw@yamanote.proxy.rlwy.net:12260'
 };
